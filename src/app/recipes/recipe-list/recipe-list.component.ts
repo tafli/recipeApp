0a8b012463ec763fdcd83b8a22d6da44c3fd0d78
Andreas Boss <andi@theboss.ch>
2018-09-23 14:10:01 +0200

    Created detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: [ './recipe-list.component.css' ]
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'http://dolerecipe.dolesunshine.com/RecipeImages/1593/Dole_Pineapple%20Red%20Velvet%20Cake%20192x192.jpg'
+    ),
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'http://dolerecipe.dolesunshine.com/RecipeImages/1593/Dole_Pineapple%20Red%20Velvet%20Cake%20192x192.jpg'
+    )
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
