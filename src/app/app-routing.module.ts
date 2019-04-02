71b691517b72ab0305c74b1444c347e8dfed5fd1
Andreas Boss <andi@theboss.ch>
2019-04-02 22:07:32 +0200

    Sign up and sign in with firebase SDK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,21 +5,27 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {path: 'recipes', component: RecipesComponent, children: [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {
+    path: 'recipes', component: RecipesComponent, children: [
       {path: '', component: RecipeStartComponent},
       {path: 'new', component: RecipeEditComponent},
       {path: ':id', component: RecipeDetailComponent},
       {path: ':id/edit', component: RecipeEditComponent},
-    ] },
-  {path: 'shopping-list', component: ShoppingListComponent },
+    ]
+  },
+  {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'signin', component: SigninComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(appRoutes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule {
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBJATWSx7kdUaW4BVjqxgFVvWp1a4jdRa8',
+      authDomain: 'udemy-recipe-book-5f8ce.firebaseapp.com'
+    };
+    firebase.initializeApp(firebaseConfig);
   }
 }
